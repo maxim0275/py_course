eca065f66831f7f433a675ac253a1fd0c2658ac7
maxim0275 <maxim0275@yandex.ru>
2025-06-30 21:03:52 +0500

    init

--- src/masks.py
+++ src/masks.py
@@ -6,7 +6,7 @@ def get_mask_card_number(card_number: str) -> str:
     card_number_masked = ""
     if len(card_number) == len("7000792289606361"):
         card_number_masked = (
-            card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[len(card_number) - 4 :]
+                card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[len(card_number) - 4:]
         )
     return card_number_masked
 
@@ -17,5 +17,5 @@ def get_mask_account(account: str) -> str:
     """
     account_masked = ""
     if len(account) == len("73654108430135874305"):
-        account_masked = "**" + account[len(account) - 4 :]
+        account_masked = "**" + account[len(account) - 4:]
     return account_masked
