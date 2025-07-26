6e61f6072106b2ae7c99eefada60d7f05ac46e10
maxim0275 <maxim0275@yandex.ru>
2025-07-26 08:17:50 +0500

    tests added

--- src/generators.py
+++ src/generators.py
@@ -28,12 +28,12 @@ def transaction_descriptions(transactions: list) -> Generator[str | Any, None, s
     for i in transactions:
         yield i["description"]
     return None
-#
-#
-# def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
-#     """ Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X  — цифра номера карты.
-#     Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
-#     Генератор должен принимать начальное и конечное значения для генерации диапазона номеров."""
-#     for i in range(start, stop):
-#         card_number = str(i).zfill(16)
-#         yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+
+
+def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
+    """ Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X  — цифра номера карты.
+    Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
+    Генератор должен принимать начальное и конечное значения для генерации диапазона номеров."""
+    for i in range(start, stop):
+        card_number = str(i).zfill(16)
+        yield f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
