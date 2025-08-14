944747bb2186ee4a7e57643ad29f11045eb531a9
maxim0275 <maxim0275@yandex.ru>
2025-08-14 10:06:09 +0500

    correct decorator and tests 14/08/2025

--- src/decorators.py
+++ src/decorators.py
@@ -1,4 +1,4 @@
-from datetime import time
+from datetime import datetime
 from functools import wraps
 
 
@@ -13,31 +13,43 @@ def log(filename=None):
         @wraps(func)
         def wrapper(*args, **kwargs):
             try:
-                time_1 = time()
-                time_2 = time()
+                time_1 = datetime.now().time()
+                time_2 = "None"
                 result = func(*args, **kwargs)
+                time_2 = datetime.now().time()
                 name_func = func.__name__
                 if filename:
-                    file = open(filename, "a", encoding="utf-8")
-                    file.write(f"Начало: {time_1}" + "\n")
-                    file.write(f"Функция {name_func} ок. Результат: {result}" + "\n")
-                    file.write(f"Конец: {time_2}" + "\n")
-                    file.write("\n")
-                    file.close()
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"Начало: {time_1}" + "\n")
+                        file.write(f"Функция {name_func} ок. Результат: {result}" + "\n")
+                        file.write(f"Конец: {time_2}" + "\n")
+                        file.write("\n")
                 else:
                     print(f"Начало: {time_1}")
-                    print(f"{name_func} ok. Результат: {func(*args, **kwargs)}")
+                    print(f"Функция {name_func} ок. Результат: {result}" + "\n")
+                    print(f"Конец: {time_2}")
+            except ZeroDivisionError:
+                result = "faulty_function error: division by zero"
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"Начало: {time_1}")
+                        file.write(f"{func.__name__} error: ZeroDivisionError. Inputs: {args}, {kwargs}")
+                        file.write(f"Конец: {time_2}")
+                else:
+                    print(f"Начало: {time_1}")
+                    print(f"{func.__name__} error: ZeroDivisionError. Inputs: {args}, {kwargs}")
                     print(f"Конец: {time_2}")
             except Exception as e:
                 result = None
-                print(f"Начало: {time_1}")
-                print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
-                print(f"Конец: {time_2}")
-            except ZeroDivisionError:
-                result = None
-                print(f"Начало: {time_1}")
-                print(f"{func.__name__} error: ZeroDivisionError. Inputs: {args}, {kwargs}")
-                print(f"Конец: {time_2}")
+                if filename:
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write(f"Начало: {time_1}")
+                        file.write(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                        file.write(f"Конец: {time_2}")
+                else:
+                    print(f"Начало: {time_1}")
+                    print(f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}")
+                    print(f"Конец: {time_2}")
             return result
 
         return wrapper
--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -1,32 +1,31 @@
-import os
-#import pytest
-from src.decorators import log
-
-
 import pytest
 
 from src.decorators import log
 
 
 def test_log():
-    @log(filename='my_log.txt')
+    @log(filename="my_log.txt")
     def summa(x, y):
         return x + y
+
     result_summa = summa(1, 2)
     assert f"Функция my_function ок. Результат: {result_summa}" == "Функция my_function ок. Результат: 3"
 
     def sb(x, y):
         return x - y
+
     result_sb = sb(2, 1)
     assert f"Функция my_function ок. Результат: {result_sb}" == "Функция my_function ок. Результат: 1"
 
     def division_corr(x, y):
         return x / y
+
     result_division_corr = division_corr(4, 2)
     assert f"Функция my_function ок. Результат: {result_division_corr}" == "Функция my_function ок. Результат: 2.0"
 
     def div_zero(x, y):
         return x / y
+
     with pytest.raises(ZeroDivisionError):
         div_zero(2, 0)
 
@@ -42,27 +41,32 @@ def test_decorator_cupsys(capsys):
 @log()
 def faulty_function(x, y):
     return x / y
+
+
 def test_faulty_function_logs_error(capsys):
-    faulty_function(1, 0)
-    captured = capsys.readouterr()
-    assert "faulty_function error: division by zero" in captured.out
+    result = faulty_function(1, 0)
+    assert result == "faulty_function error: division by zero"
+
 
 @log()
 def my_function(x, y):
     return x / y
 
+
 # Тест на успешное выполнение функции
 
+
 def test_my_function_success(capsys):
-    result = my_function(4, 2)
-    assert result == 2
+    my_function(4, 2)
     captured = capsys.readouterr()
-    assert "my_function ok. Результат: 2" in captured.out
+    assert "Функция my_function ок. Результат: 2.0" in captured.out
+
 
 # Тест на обработку ошибки деления на ноль
 
-#def test_my_function_division_by_zero(capsys):
-#    with pytest.raises(ZeroDivisionError):
-#        my_function(4, 0)
-#    captured = capsys.readouterr()
-#    assert "my_function error: division by zero. Inputs: (4, 0), {}" in captured.out
+
+def test_my_function_division_by_zero(capsys):
+    with pytest.raises(Exception):
+        my_function(1, 0)
+        captured = my_function.readouterr()
+        assert "ZeroDivisionError" in captured.out
