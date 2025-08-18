314786a71534517bcf8dd084819e5f6aeeed02e6
maxim0275 <maxim0275@yandex.ru>
2025-08-18 10:14:56 +0500

    commit

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,40 @@
+import json
+
 import requests
 
-def get_github_users():
-    pass
+
+def get_github_users(users):
+    results = []
+    for user in users:
+        status, user_data = get_user_info(user)
+
+    return 1
+
+
+def get_user_info(user):
+    result = requests.get(f"https://api.github.com/users/{user}")
+    if result.status_code != 200:
+        status = False
+    else:
+        status = True
+
+    return "1", "1"
+
+
+def get_fin_data(path_to_file):
+    """
+    принимает на вход путь до JSON-файла и
+    возвращает список словарей с данными о финансовых транзакциях
+    """
+    transactions = []
+    try:
+        with open(path_to_file, 'r', encoding='utf-8', errors='ignore') as f:
+            transactions = json.load(f)
+    except Exception as e:
+        print(f"Ошибка чтения файла.{e}")
+
+    transactions = [transaction for transaction in transactions]
+
+    return transactions
+
+
